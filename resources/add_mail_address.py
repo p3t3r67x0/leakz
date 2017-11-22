21dfc24c550716f8c35cdb3e8b3dc440569a5c7a
Aurelius Wendelken <webtobesocial@gmail.com>
2017-11-22 12:11:20 +0100

    added method to update entries and added leak field

--- resources/add_mail_address.py
+++ resources/add_mail_address.py
@@ -26,13 +26,20 @@ def connect_database():
     return client.hashes
 
 
-def insert_one(collection, mail_address):
+def insert_one(collection, mail_address_string, leak_name):
     try:
-        mail_address_string = mail_address.strip('\n').strip('\r').lower()
-        inserted_id = collection.insert_one({ 'mail': mail_address_string }).inserted_id
+        inserted_id = collection.insert_one({ 'mail': mail_address_string,
+                                             'leak': [ leak_name ] }).inserted_id
         print u'[I] Added {} with id {}'.format(mail_address_string.decode('utf-8'), inserted_id)
     except pymongo.errors.DuplicateKeyError as e:
-        print u'{}'.format(e)
+        find_one_and_update(collection, mail_address_string, leak_name)
+
+
+def find_one_and_update(collection, mail_address_string, leak_name):
+    result = collection.find_one_and_update({ 'mail': mail_address_string, },
+                                   { '$addToSet': { 'leak': leak_name }} )
+
+    print u'[I] Updated {} with id {}'.format(result['mail'], result['_id'])
 
 
 def extract_mail_address(document):
@@ -40,17 +47,18 @@ def extract_mail_address(document):
 
 
 def main():
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         db = connect_database()
         db.mail_address.create_index('mail', unique=True)
         collection = db.mail_address
 
         document = ' '.join(load_document(sys.argv[1]))
         mail_address_list = extract_mail_address(document)
+        leak_name = sys.argv[2]
 
         for mail_address in mail_address_list:
-            mail_address = handle_unicode(mail_address.strip('\n').strip('\r'))
-            insert_one(collection, mail_address)
+            mail_address = handle_unicode(mail_address.strip('\n').strip('\r').lower())
+            insert_one(collection, mail_address, leak_name)
 
 
 if __name__ == '__main__':
