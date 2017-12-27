6c864f5235631f9bac0c7f77fb00a8c6e4e14116
Aurelius Wendelken <webtobesocial@gmail.com>
2017-12-27 15:11:52 +0100

    added multiprocessing for password hashing

---
+++ resources/multi_add_password.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import sys
+import math
+import multiprocessing
+import add_password
+
+
+def load_document(filename):
+    try:
+        with open(filename, 'rb') as f:
+            return f.readlines()
+    except IOError as e:
+        print e
+        sys.exit(1)
+
+
+def worker(passwords):
+    db = add_password.connect_database()
+    collection = db.password
+
+    try:
+        collection.create_index("password", unique=True)
+        collection.create_index("hash.md5", unique=True)
+        collection.create_index("hash.sha1", unique=True)
+        collection.create_index("hash.sha224", unique=True)
+        collection.create_index("hash.sha256", unique=True)
+        collection.create_index("hash.sha384", unique=True)
+        collection.create_index("hash.sha512", unique=True)
+    except pymongo.errors.OperationFailure as e:
+        print e
+        sys.exit(1)
+
+    for password in passwords:
+        password = password.strip()
+
+        if password and not add_password.extract_mail_address(password):
+            password_string = add_password.handle_unicode(password)
+
+            if len(password_string) > 3 and len(password_string) < 24:
+                hash_string = add_password.hash_password(password)
+                add_password.insert_one(collection, password_string, hash_string)
+
+    return
+
+
+def main():
+    passwords = load_document(sys.argv[1])
+    core_count = multiprocessing.cpu_count()
+    chunk_size = int(math.ceil(len(passwords) / core_count))
+    jobs = []
+
+    for i in xrange(core_count):
+        job = passwords[i * chunk_size:(i + 1) * chunk_size]
+        p = multiprocessing.Process(target=worker, args=(job,))
+        jobs.append(p)
+        p.start()
+
+
+if __name__ == '__main__':
+    main()
