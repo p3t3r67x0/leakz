4283b9cc186004fc10fe57bef1f7f2d51a09d0f4
Aurelius Wendelken <webtobesocial@gmail.com>
2018-01-02 16:11:56 +0100

    refactored and used utils to stay dry

--- resources/update_mail_address.py
+++ resources/update_mail_address.py
@@ -2,47 +2,34 @@
 # -*- coding:utf-8 -*-
 
 import os
+import json
 import pymongo
 
+from pymongo.errors import DuplicateKeyError
 
-def connect_database():
-    secret = get_secret()
-    client = pymongo.MongoClient('mongodb://localhost:27017/',
-             username='pymongo',
-             password=secret,
-             authSource='hashes',
-             authMechanism='SCRAM-SHA-1')
-
-    return client.hashes
-
-
-def get_secret():
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../.secret'))
-    return load_document(path)[0].strip()
+import utils.database_helper as dbh
+import utils.password_handling as ph
+import utils.file_handling as fh
 
 
 def update_one(collection, document_id, post):
     try:
-        collection.update_one({'_id': document_id}, {"$set": post}, upsert=False)
-    except pymongo.errors.DuplicateKeyError as e:
+        collection.update_one({'_id': document_id}, {
+                              "$set": post}, upsert=True)
+    except DuplicateKeyError as e:
         print u'{}'.format(e)
 
 
-def find_all_documents(collection):
-    return collection.find({})
-
-
 def main():
-    db = connect_database()
+    config = json.loads(fh.get_config())
+    db = dbh.connect_database('hashes', config['db_port_mail'])
     collection = db.mail_address
-
-    documents = find_all_documents(collection)
+    documents = dbh.find_all_documents(collection)
 
     for document in documents:
         document_id = document['_id']
-        mail_address = document['mail'].strip('\n').strip('\r')
 
-        post = { 'mail': mail_address }
+        post = {'mail': ph.remove_whitespace(document['mail'])}
         update_one(collection, document_id, post)
 
 
