18b3d8b9c5fa5755a400b69ed684620c22781ae4
Aurelius Wendelken <webtobesocial@gmail.com>
2018-01-07 13:27:14 +0100

    use collection var

--- resources/utils/database_helper.py
+++ resources/utils/database_helper.py
@@ -9,7 +9,7 @@ import file_handling as fh
 
 
 def delete_one(collection, document_id, password):
-    result = db[collection._Collection__name].delete_one({'_id': ObjectId(document_id)})
+    result = collection.delete_one({'_id': ObjectId(document_id)})
     print result.deleted_count
     print u'[I] deleted {} from collection: {}'.format(password, collection._Collection__name)
 
