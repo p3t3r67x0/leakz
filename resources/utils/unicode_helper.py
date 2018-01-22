0354c8e36be42754eb04763a2a720500476248a7
Aurelius Wendelken <webtobesocial@gmail.com>
2018-01-22 22:29:21 +0100

    set empty string when an exception raises

--- resources/utils/unicode_helper.py
+++ resources/utils/unicode_helper.py
@@ -9,7 +9,6 @@ def handle_unicode(password):
         try:
             password_string = password.encode('utf-8')
         except UnicodeDecodeError as e:
-            password_string = password.decode('ascii')
-            print u'{}'.format(e)
+            password_string = ''
 
     return password_string
