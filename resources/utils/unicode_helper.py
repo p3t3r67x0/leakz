41f397acbe70b890ead6b63fb7ba8aef68bfe396
Aurelius Wendelken <webtobesocial@gmail.com>
2018-10-16 17:43:26 +0200

    improved unicode handling

--- resources/utils/unicode_helper.py
+++ resources/utils/unicode_helper.py
@@ -6,9 +6,8 @@ def handle_unicode(password):
     try:
         password_string = password.decode('utf-8')
     except UnicodeDecodeError as e:
-        try:
             password_string = password.encode('utf-8')
-        except UnicodeDecodeError as e:
-            password_string = ''
+    except UnicodeEncodeError as e:
+            password_string = password.encode('ascii', 'ignore')
 
     return password_string
