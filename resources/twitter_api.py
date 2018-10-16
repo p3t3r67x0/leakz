5924a36e8e906815ed4243ac1fc15a9212d90e7f
Aurelius Wendelken <webtobesocial@gmail.com>
2018-10-16 17:37:19 +0200

    retrieve tweets from checkmydump and extract mail and passwords

--- resources/twitter_api.py
+++ resources/twitter_api.py
@@ -7,7 +7,15 @@ import twitter
 import requests
 import re
 
+from urlparse import urlparse
+
+from pymongo.errors import WriteError
+from pymongo.errors import BulkWriteError
+from pymongo.errors import DuplicateKeyError
+from pymongo.errors import OperationFailure
+
 import utils.database_helper as dbh
+import utils.password_handling as ph
 import utils.unicode_helper as uh
 import utils.file_handling as fh
 import utils.mail_handling as mh
@@ -20,17 +28,26 @@ def connect_twitter(config):
                       access_token_secret = config['access_token_secret'])
 
 
-def insert_one(collection, mail_address_string, leak_name):
+def insert_password(col_password, password_string, hash_string):
     try:
-        inserted_id = collection.insert_one({'mail': mail_address_string,
+        inserted_id = col_password.insert_one(
+            {'password': password_string, 'hash': hash_string}).inserted_id
+        print u'[I] Added {} with id: {}'.format(password_string, inserted_id)
+    except (DuplicateKeyError, WriteError) as e:
+        print u'[E] {}'.format(e)
+
+
+def insert_mail(col_mail, mail_address, leak_name):
+    try:
+        inserted_id = col_mail.insert_one({'mail': mail_address,
                                              'leak': [leak_name]}).inserted_id
-        print u'[I] Added {} with id {}'.format(mail_address_string.decode('utf-8'), inserted_id)
-    except pymongo.errors.DuplicateKeyError as e:
-        find_one_and_update(collection, mail_address_string, leak_name)
+        print u'[I] Added {} with id {}'.format(mail_address.decode('utf-8'), inserted_id)
+    except DuplicateKeyError as e:
+        update_mail(col_mail, mail_address, leak_name)
 
 
-def find_one_and_update(collection, mail_address_string, leak_name):
-    result = collection.find_one_and_update({'mail': mail_address_string, },
+def update_mail(col_mail, mail_address, leak_name):
+    result = col_mail.find_one_and_update({'mail': mail_address, },
                                             {'$addToSet': {'leak': leak_name}})
 
     print u'[I] Updated {} with id {}'.format(result['mail'], result['_id'])
@@ -56,25 +73,40 @@ def iterate_items(statuses):
 
 def main():
     config = json.loads(fh.get_config())
-    db = dbh.connect_database(config['db_name'], config['db_port_mails'])
-    collection = db['mails']
+    db = dbh.connect_database(config['mongodb_db'], config['mongodb_port'])
+    col_password = db['passwords']
+    col_mail = db['mails']
 
     try:
-        collection.create_index('mail', unique=True)
-    except pymongo.errors.OperationFailure as e:
+        col_mail.create_index('mail', unique=True)
+        col_password.create_index("password", unique=True)
+        col_password.create_index("hash.md5", unique=True)
+        col_password.create_index("hash.sha1", unique=True)
+        col_password.create_index("hash.sha224", unique=True)
+        col_password.create_index("hash.sha256", unique=True)
+        col_password.create_index("hash.sha384", unique=True)
+        col_password.create_index("hash.sha512", unique=True)
+    except OperationFailure as e:
         print u'{}'.format(e)
         sys.exit(1)
 
     api = connect_twitter(config)
     items = get_user_timeline(api, 'checkmydump')
-    content = iterate_items(items)
+    lines = iterate_items(items)
 
-    mail_address_list = mh.extract_mail_address(''.join(content))
+    for line in lines:
+        for string in line.split('\r\n'):
+            password = ph.extract_pastebin_password(string.split('|')[0])
 
-    for mail_address in mail_address_list:
+            if not mh.extract_mail_address(password):
+                password_string = uh.handle_unicode(password)
+
+                if len(password_string) > 3 and len(password_string) < 32 and not ph.test_md5(password_string):
+                    insert_password(col_password, password_string, ph.hash_password(password_string))
+
+    for mail_address in mh.extract_mail_address(''.join(lines)):
         mail_address = uh.handle_unicode(mail_address.strip().lower())
-        insert_one(collection, mail_address, 'pastebin.com')
-
+        insert_mail(col_mail, mail_address, 'pastebin.com')
 
 
 if __name__ == '__main__':
